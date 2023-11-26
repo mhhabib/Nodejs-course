49317a8e95dcd6d5768a5ba03323aa92cab3d625
mhhabib <mhrahman7096@gmail.com>
2023-11-26 22:12:26 +0600

    added template engine ejs

--- routes/shop.js
+++ routes/shop.js
@@ -7,7 +7,14 @@ const router = express.Router();
 
 router.get("/", (req, res, next) => {
 	console.log("shop.js", adminData.products);
-	res.sendFile(path.join(rootDir, "views", "shop.html"));
+	res.render("shop", {
+		allProducts: adminData.products,
+		pageTitle: "Shop",
+		path: "/",
+		hasProducts: adminData.products.length > 0,
+		activeShop: true,
+		productCSS: true,
+	});
 });
 
 module.exports = router;
